0a5663d4245c5e8697c32b03e144bc20404115b4
milobedini <milobedini64@gmail.com>
2023-02-21 11:42:04 +0000

    allow thread body text to be blank

--- articles/models.py
+++ articles/models.py
@@ -8,7 +8,7 @@ class Article(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=200)
     views = models.IntegerField(default=0)
-    text = models.TextField(default='')
+    text = models.TextField(default='', null=True, blank=True)
     category = models.CharField(max_length=30, default="Threads")
     photo = models.CharField(max_length=200, default='')
     author = models.ForeignKey(
